f68269d35c099ecdde6cb5f81d7e0379b78c4aa3
Jeffrey King <jdking33@aol.com>
2017-02-06 09:34:32 -0800

    Submit 3-assignment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,9 @@ class BooksController < Frame::Controller
   def index
     render :index, books: Book.all
   end
+
+  def show(arg)
+    book = Book.find(arg)
+    render :show, book: book
+  end
 end
